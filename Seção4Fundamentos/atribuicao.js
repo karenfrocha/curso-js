56ed5b182590d3582822d3457760e815529e8e70
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-17 19:28:42 -0300

    feat: add atribuicao.js

---
+++ Seção4Fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 //o = é uma atribuição
+let b = 3
+
+b += a // b recebe o valor de b mais o de a
+console.log(b)
+
+b -= 4 // b recebe o valor dele menos 4
+console.log(b)
+
+b *= 2
+console.log(b)
+
+b /= 2
+console.log(b)
+
+b %= 2 // b recebe o valor de b mais modulo 2
+console.log(b)
