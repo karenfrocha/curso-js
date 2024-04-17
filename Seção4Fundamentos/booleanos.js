3e1b1911e0e938ad64c8a80c68af8efb8618184c
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-17 19:29:24 -0300

    feat: add booleanos.js

---
+++ Seção4Fundamentos/booleanos.js
@@ -0,0 +1,30 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('os falsos')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('pra finalizar')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = '' // se vc colocar um nome aqui ele vai imprimir no console o nome e não o 'desconhecido'
+console.log(nome || 'Desconhecido')
