e5b91b35b97bf5003cb334d6bedf86d4c1d39975
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-29 21:09:43 -0300

    feat: add objeto.js

---
+++ Seção4Fundamentos/objeto.js
@@ -0,0 +1,19 @@
+const prod1 = {}        //chave {} criam objetos, [] colchetes criam arreios Array
+prod1.name = 'Celular Ultra Mega'
+prod1.preco = 4999.90
+prod1['Desconto Legal'] = 0.40 //evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    name: 'Camisa Polo',
+    preco: 79.90,
+    /*obj: {
+        coisa: 1,
+        obj: {
+            coisa: 2
+        }
+    } */
+}
+
+console.log(prod2)
