779474d23098f67f77de41842a484caabc54f403
Karen Félix <karen.felix.rocha@gmail.com>
2024-05-07 21:49:09 -0300

    feat: add usandoLetEmLoop.js

---
+++ Seção4Fundamentos/usandoLetEmLoop.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+console.log('i =', i)
+
+//for usado para looping enquanto tal condição não for atendida o looping não para
+// e let não funciona fora do bloco se ela foi posta para ser usada no bloco.
