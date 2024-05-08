2efcb59ead388632b7d0917725246f86263bb796
Karen Félix <karen.felix.rocha@gmail.com>
2024-05-07 21:49:49 -0300

    feat: add usandoVar.js

---
+++ Seção4Fundamentos/usandoVar.js
@@ -0,0 +1,21 @@
+{
+    {
+        {
+            {
+                var sera = 'sera???'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+//console.log(local) // só pode ser chamada dentro da função a var local
+//variavel só tem dois escopos, ou é visivel para toda a aplicação ou ela é var dentro da função
