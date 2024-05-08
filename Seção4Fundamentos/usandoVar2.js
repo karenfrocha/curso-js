9fb0223b4c51bd44d3d38b029a6fa4f5b4f0c9bb
Karen Félix <karen.felix.rocha@gmail.com>
2024-05-07 21:50:15 -0300

    feat: add usandoVar2.js

---
+++ Seção4Fundamentos/usandoVar2.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+/*var em bloco sem função é considerada como uma var que foi alterada. 
+Ela só fica restrita quando é bloco de função.*/
