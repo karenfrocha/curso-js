7bac13db1fc4b9d4a5efaffa8a9118ee92af9629
Karen Félix <karen.felix.rocha@gmail.com>
2024-05-20 13:03:51 -0300

    feat: add while.js

---
+++ Seção6ExtruturasDeControle/while.js
@@ -0,0 +1,14 @@
+//repetições longas, "infinitas", laço, usado quando não se tem uma quantidade de repetições definidas
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima')
