075c08da8872b71d3b593b72064dbcce57dfd287
Karen Félix <karen.felix.rocha@gmail.com>
2024-05-28 11:31:21 -0300

    feat: add OO.js

---
+++ Seção7Objeto/OO.js
@@ -0,0 +1,25 @@
+//CÓDIGO NAO EXECUTÁVEL
+
+// Paradigma Procedural
+processamento(valor1, valor2, valor3)
+
+//OO = orientação à objeto
+//objetos tem valores, comportamentos e se relacionam entre si
+objeto = {
+    valor1, 
+    valor2,
+    valor3,
+    processamento() {
+        // .....
+    }
+}
+
+objeto.processamento() // dados que tem funções, foco passa a ser o objeto
+
+/*Princípios Importantes:
+1. Abstração, pegar o objeto no mundo real e trazer para o software
+2. Encapsulamento, deixar visivel apenas uma interface simples, o funcionamento está "escondido"/encapsulado,
+detalhes de implementação.
+3. Herança, reuso de código, recebe atributos e comportamentos de um objeto pai
+4. Polimorfismo, ex: carro é genérico, depois vem as várias formas um carro ferrari, um carro uno, um civic
+*/
